ebfb440b7a44f0ccd8efca439e92b6be8611d05e
Pepri <pepriandika@gmail.com>
2025-01-11 07:47:16 +0700

    Add Photo in AboutMe page

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -9,22 +9,39 @@ const goContact = () => {
 </script>
 
 <template>
-
   <v-container>
-    <v-row>
-      <v-col>
-        <h1>Hai! Saya Pepri Andika <br/> Saya Adalah Front End Developer</h1>
+    <v-row class="justify-space-around">
+      <v-col align-self="center" :cols="6">
+        <h1>Hai! Saya <span style="background-color: #4a8d9e; color: white;"> Pepri Andika </span><br/> Saya Adalah Front End Developer</h1>
+        <br/>
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid aut corporis doloremque, eaque eum exercitationem id illo in iusto laboriosam laborum libero neque, nobis quae repudiandae sit velit voluptatem voluptatum!</p>
         <button @click="goContact">Kembali ke Home</button>
       </v-col>
 
-      <v-col :cols="4" class="about-photo border-b">
-        <img src="@/assets/logo.png" alt="My Photo" srcset="">
+      <v-col class="about-photo ">
+        <div class="border-photo"><img src="../assets/peps-ton.png" alt="My Photo" class="my-photo" width="638" height="784"></div>
+
       </v-col>
     </v-row>
-
   </v-container>
 
 </template>
 
+<style scoped>
+  .about-photo{
+    flex-grow: 0;
+    border-radius: 2px!important;
+    border-color: #4a8d9e;
+  }
 
+  .my-photo{
+    border-radius: 50%;
+    border: 2px #1a1e1e;
+    width: 300px;
+    height: auto;
+  }
+
+  .my-photo:hover{
+
+  }
+</style>
